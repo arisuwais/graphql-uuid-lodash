6c4f9387a9c3671bccdfc2496b2f9e9e895b4a47
aris uwais <aris.septianto@ymail.com>
2019-02-06 01:52:58 -0500

    first

---
+++ src/graphql/mutations.js
@@ -0,0 +1,32 @@
+const uuidv4 = require('uuid/v4');
+const { GraphQLNonNull, GraphQLObjectType } = require('graphql');
+const {
+    BookType,
+    BookCreateType
+} = require('./types.js');
+const Books = require('../../data/books.js');
+
+
+//...import statements...
+const BookMutationType = new GraphQLObjectType({
+    name: 'BookMutationType',
+    description: 'Mutations for BookType',
+    fields: {
+        createBook: {
+            type: BookType,
+            args: {
+                input: { type: new GraphQLNonNull(BookCreateType) }
+            },
+            resolve: (source, { input }) => {
+                let book = [];
+                book.id = uuidv4();
+                book.name = input.name;
+                book.author = input.author;
+                Books.push(book);
+                return book;
+            }
+        }
+    }
+});
+
+module.exports = BookMutationType;
